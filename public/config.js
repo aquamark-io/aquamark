8cd03c9322b2e3bb0ff76eec288dfeecc1ecea4b
aquamark-io <heyaquamark@gmail.com>
2025-04-15 19:23:22 -0700

    Update config.js

--- public/config.js
+++ public/config.js
@@ -1,8 +1,8 @@
 // config.js - Place this in your public folder
 const config = {
   // Supabase credentials
-  supabaseUrl: 'your_supabase_url_here',
-  supabaseKey: 'your_supabase_anon_key_here',
+  supabaseUrl: 'https://dvzmnikrvkvgragzhrof.supabase.co',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR2em1uaWtydmt2Z3JhZ3pocm9mIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM5Njg5NzUsImV4cCI6MjA1OTU0NDk3NX0.FaHsjIRNlgf6YWbe5foz0kJFtCO4FuVFo7KVcfhKPEk',
   
   // Outseta domain
   outsetaDomain: 'aquamark.outseta.com',
