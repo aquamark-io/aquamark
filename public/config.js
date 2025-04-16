8f669717420eb139384b85a951aa240efa1bb6ce
aquamark-io <heyaquamark@gmail.com>
2025-04-15 19:18:17 -0700

    Create config.js

---
+++ public/config.js
@@ -0,0 +1,17 @@
+// config.js - Place this in your public folder
+const config = {
+  // Supabase credentials
+  supabaseUrl: 'your_supabase_url_here',
+  supabaseKey: 'your_supabase_anon_key_here',
+  
+  // Outseta domain
+  outsetaDomain: 'aquamark.outseta.com',
+  
+  // Plan IDs
+  plans: {
+    freeTrial: 'aWxLDdWV',
+    ridingSolo: 'wQXVMxWK',
+    smallTeams: 'L9P2krmJ',
+    isoShops: 'xmeJ6nQV'
+  }
+};
